0940e2d53d762dbffc218b64b392631a409f9178
johntrecker <jttrecker@gmail.com>
2016-06-13 15:08:49 -0700

    Complete Grub, Bee, HoneyMakerBee, ForagerBee classes

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
+HoneyMakerBee.prototype.eat = function() {
+  Grub.prototype.eat.call(this);
+};
