49acad4e2a4ef3b2ea9f1e1a61302a5518a0b04a
johntrecker <jttrecker@gmail.com>
2016-06-13 15:17:52 -0700

    Complete RetiredForagerBee function

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,6 +1,24 @@
 var RetiredForagerBee = function() {
-
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = "gamble";
+  this.canFly = false;
+  this.color = "grey";
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
 
+RetiredForagerBee.prototype.forage = function() {
+  return "I am too old, let me play cards instead";
+};
+
+RetiredForagerBee.prototype.eat = function() {
+  Grub.prototype.eat.call(this);
+}
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  ForagerBee.prototype.forage.call(this);
+  this.forage(treasure);
+}
 
